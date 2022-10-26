c0ed490c3003782c926f7a3423a3792bdd40060c
raz74 <razijam996@gmail.com>
2022-10-26 13:18:05 +0330

    update store-app models & add choice field class

--- store/models.py
+++ store/models.py
@@ -17,10 +17,19 @@ class product(models.Model):
 
 
 class customer(models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    MEMBERSHIP_CHOICE = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold')
+    ]
+
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=225)
-    phone = models.IntegerField(max_length=11)
+    phone = models.CharField(max_length=11)
     birth_date = models.DateField(null=True)
 
 
@@ -36,11 +45,32 @@ class collection(models.Model):
 
 
 class order(models.Model):
-    pass
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F'
+    PAYMENT_STATUS_CHOICE = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICE, default=PAYMENT_STATUS_PENDING)
 
 
 class orderItem(models.Model):
     order = models.ForeignKey(order, on_delete=models.PROTECT)
-    product = models.ForeignKey(order, on_delete=models.PROTECT)
+    product = models.ForeignKey(product, on_delete=models.PROTECT)
     quantity = models.PositiveIntegerField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=None)
+    # for saving change the price
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+
+class cart(models.Model):
+    creat_at = models.DateTimeField(auto_now_add=True)
+
+
+class cartItem(models.Model):
+    cart = models.ForeignKey(cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
